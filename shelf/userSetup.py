23104e289f15a9ddf326e18fc6f79703ef725f09
RobRuckus <robcoakley@gmail.com>
2018-01-23 20:03:14 -0500

    update

---
+++ shelf/userSetup.py
@@ -0,0 +1,9 @@
+import os 
+import sys 
+def userDirectory():
+	if 'darwin' in sys.platform:
+		userDirectory=os.environ['HOME']
+	else:
+		userDirectory=os.environ['USERPROFILE']
+	return userDirectory
+sys.path.append(os.path.join(userDirectory()))
